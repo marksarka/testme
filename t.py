d9cd51bbe2e03f625ec9e95621c8c190fa403baf
marksarka <66681979+marksarka@users.noreply.github.com>
2024-07-25 20:38:20 +0300

    Update t.py
    Co-authored-by: jit-ci[bot] <91912817+jit-ci[bot]@users.noreply.github.com>

--- t.py
+++ t.py
@@ -1,4 +1,4 @@
 import subprocess
-output = subprocess.check_output(f"nslookup2 {my_domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup2 {my_domain}", shell=False, encoding='UTF-8')
 
 a=""
