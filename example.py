ac39b61be19a3eaecb33e51087913ea0d8333274
marksarka <66681979+marksarka@users.noreply.github.com>
2025-01-27 11:29:51 +0200

    Update example.py
    Co-authored-by: jit-ci[bot] <91912817+jit-ci[bot]@users.noreply.github.com>

--- example.py
+++ example.py
@@ -6,4 +6,4 @@ def exploitable_yaml_load(**kwargs):
     to create a new Python object by calling 'os.system' with 'echo EXPLOIT!' as its argument.
     When using 'yaml.unsafe_load', this can lead to arbitrary command execution.
     """
-    yaml.unsafe_load("!!python/object/new:os.system [echo EXPLOIT!]", **kwargs)
+    yaml.safe_load("!!python/object/new:os.system [echo EXPLOIT!]", **kwargs)
